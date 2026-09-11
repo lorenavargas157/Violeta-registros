9588122a6efeeeeb33bf69e1135866838e85ac69
Violeta Registro <lore@violeta-registro.local>
2026-09-11 04:18:49 +0000

    Agregar item de extraccion y alertas para los 4 tipos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'violeta-v3';
+const CACHE = 'violeta-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
